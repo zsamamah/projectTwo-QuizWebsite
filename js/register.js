ad015bc69b595d6bc56945a9ffde93ba8ad7e58a
farah-habarnih <farah.habarnih@gmail.com>
2021-10-23 23:25:00 +0300

    functionality done

--- js/register.js
+++ js/register.js
@@ -1,32 +1,31 @@
-//Navbar 
 
-function register(){
-    let fname = document.getElementById('fname').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
+let fname = document.getElementById('fname');
+let lname = document.getElementById('lname');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+
+let user_data={};
+let all_user_data=[];
+
+document.querySelector('form').addEventListener("submit",function(e){
+    e.preventDefault();
+
+    user_data={
+        first_name:fname.value,
+        last_name:lname.value,
+        email:email.value,
+        password:password.value,
+    };
+
+    all_user_data.push(user_data);
+    localStorage.setItem('users_accounts',JSON.stringify(all_user_data));
+
+    fname.value="";
+    lname.value="";
+    email.value="";
+    password.value="";
+
+    window.location.href="./index.html";
+});
     
-    
-    
-    localStorage.setItem("fname",fname);
-    localStorage.setItem("email",email);
-    localStorage.setItem("password",password);
-    
-    }
-    
-    
-    //Registration form
-    
-    
-        function Register(fname,email,password){
-            this.email=email;
-            this.fname=fname;
-            this.password=password;
-            }
-            
-    //         Register.holder= [];
-            
-    //         localStorage.setItem('data', JSON.stringify(Register.holder));
-            
-            
-    //         console.log(Register.holder);
-    
+  
