821f190fe036c7dd9f5cbf9557c9e22cde45df15
farah-habarnih <farah.habarnih@gmail.com>
2021-10-25 15:50:07 +0300

    Register Page Done

--- js/register.js
+++ js/register.js
@@ -33,7 +33,7 @@ function check_function() {
 let fname = document.getElementById("fname");
 let lname = document.getElementById("lname");
 let email = document.getElementById("email");
-let password = document.getElementById("password");
+let password = document.getElementById("password1");
 
 document.querySelector(".toggle-btn").addEventListener("click", () => {
   document.querySelector(".nav-list").classList.toggle("hidden");
@@ -42,7 +42,7 @@ document.querySelector(".toggle-btn").addEventListener("click", () => {
 
 const all_user_data = [];
 
-document.querySelector("form").addEventListener("submit", function (e) {
+document.querySelector("#form").addEventListener("submit", function (e) {
   e.preventDefault();
 
   const user_data = {
@@ -69,3 +69,29 @@ document.querySelector("form").addEventListener("submit", function (e) {
 
   window.location.href = "./index.html";
 });
+// function moveToLogin(e){
+//     e.preventDefault();
+//     const user_data = {
+//         first_name: fname.value,
+//         last_name: lname.value,
+//         email: email.value,
+//         password: password.value,
+//       };
+    
+//       all_user_data.push(user_data);
+    
+//       if (!localStorage.getItem("users_accounts")) {
+//         localStorage.setItem("users_accounts", JSON.stringify(all_user_data));
+//       } else {
+//         let getAcconuts = JSON.parse(localStorage.getItem("users_accounts"));
+//         getAcconuts.push(user_data);
+//         localStorage.setItem("users_accounts", JSON.stringify(getAcconuts));
+//       }
+    
+//       fname.value = "";
+//       lname.value = "";
+//       email.value = "";
+//       password.value = "";
+    
+//       window.location.href = "./index.html";    
+// }
